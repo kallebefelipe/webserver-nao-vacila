19a714566e3e0fddee587103ecb7e554e5cc67d8
Kallebe Felipe <kallebefelipe@gmail.com>
2017-07-01 14:01:29 -0300

    Correcao

--- nao_vacila/urls.py
+++ nao_vacila/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+# urlpatterns = [
+#     url(r'^admin/', admin.site.urls),
+# ]
 
 urlpatterns = [
     url(r'^', include('ocorrencia.urls')),
