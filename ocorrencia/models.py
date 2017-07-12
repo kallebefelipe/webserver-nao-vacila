eebb1bcc328d90fd1ef34b34c4fa91945b53c043
Kallebe Felipe <kallebefelipe@gmail.com>
2017-07-12 14:36:51 -0300

    Remodelagem de ocorrencia

--- ocorrencia/models.py
+++ ocorrencia/models.py
@@ -13,20 +13,30 @@ class Ocorrencia(Base):
     __tablename__ = 'ocorrencia'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
+    data = Column(String, nullable=True)
+    hora = Column(String, nullable=True)
+    titulo = Column(String, nullable=True)
+    id_usuario = Column(String, nullable=True)
+    latitude = Column(String, nullable=True)
+    longitude = Column(String, nullable=True)
     descricao = Column(String, nullable=True)
-    rua = Column(String, nullable=True)
-    tipo = Column(String, nullable=True)
-    coordenada_x = Column(String, nullable=True)
-    coordenada_y = Column(String, nullable=True)
+    id_tipo = Column(String, nullable=True)
+    endereco_rua = Column(String, nullable=True)
 
     def __init__(self, row):
+        if 'data' in row:
+            self.descricao = row['data']
+        if 'hora' in row:
+            self.hora = row['hora']
+        if 'titulo' in row:
+            self.titulo = row['titulo']
+        if 'id_usuario' in row:
+            self.id_usuario = row['id_usuario']
+        if 'longitude' in row:
+            self.longitude = row['longitude']
         if 'descricao' in row:
             self.descricao = row['descricao']
-        if 'rua' in row:
-            self.rua = row['rua']
-        if 'tipo' in row:
-            self.tipo = row['tipo']
-        if 'coodenada_x' in row:
-            self.coordenada_x = row['coordenada_x']
-        if 'coodenada_y' in row:
-            self.coordenada_y = row['coordenada_y']
+        if 'id_tipo' in row:
+            self.id_tipo = row['id_tipo']
+        if 'endereco_rua' in row:
+            self.endereco_rua = row['endereco_rua']
