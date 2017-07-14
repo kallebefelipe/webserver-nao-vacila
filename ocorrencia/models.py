d64eeadb5e9686a2db1dfdfeacaec0d0c97b3c9e
Kallebe Felipe <kallebefelipe@gmail.com>
2017-07-13 22:02:39 -0300

    Ajustes no modelo de ocorrencia

--- ocorrencia/models.py
+++ ocorrencia/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from sqlalchemy import Column
 from sqlalchemy import Integer
 from sqlalchemy import String
+from sqlalchemy import Float
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -17,15 +18,15 @@ class Ocorrencia(Base):
     hora = Column(String, nullable=True)
     titulo = Column(String, nullable=True)
     id_usuario = Column(String, nullable=True)
-    latitude = Column(String, nullable=True)
-    longitude = Column(String, nullable=True)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     descricao = Column(String, nullable=True)
     id_tipo = Column(String, nullable=True)
     endereco_rua = Column(String, nullable=True)
 
     def __init__(self, row):
         if 'data' in row:
-            self.descricao = row['data']
+            self.data = row['data']
         if 'hora' in row:
             self.hora = row['hora']
         if 'titulo' in row:
@@ -34,6 +35,8 @@ class Ocorrencia(Base):
             self.id_usuario = row['id_usuario']
         if 'longitude' in row:
             self.longitude = row['longitude']
+        if 'latitude' in row:
+            self.latitude = row['latitude']
         if 'descricao' in row:
             self.descricao = row['descricao']
         if 'id_tipo' in row:
