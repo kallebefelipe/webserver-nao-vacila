8ce0b2dbc4152639940d8bebd21db410c70c8446
Kallebe Felipe <kallebefelipe@gmail.com>
2017-08-03 19:04:59 -0300

    Adicionando estatisticas por ano e tipo

--- estatisticas/load.py
+++ estatisticas/load.py
@@ -32,13 +32,74 @@ class LoadToPostgres(PostgresConnection):
     """docstring for LoadToPostgres"""
 
     def get_estatisticas(self):
+        estatisticas = {}
         session = self.create_connection()
         count2 = (func.count(Ocorrencia.bairro)).label('count2')
-        distribuicao_bairro = session.query(Ocorrencia.bairro, count2).\
+        distribuicao = session.query(Ocorrencia.bairro, count2).\
             group_by(Ocorrencia.bairro).all()
-        distribuicao = []
-        for bairro, qt in distribuicao_bairro:
+        distribuicao_bairro = []
+        for bairro, qt in distribuicao:
             if bairro is not None:
                 if 'rua' not in bairro and 'avenida' not in bairro:
-                    distribuicao.append({'bairro': bairro, 'quantidade': qt})
-        return distribuicao
+                    distribuicao_bairro.append({'bairro': bairro, 'quantidade': qt})
+        estatisticas['distribuicao_bairro'] = distribuicao_bairro
+
+        count2 = (func.count(Ocorrencia.id_tipo)).label('count2')
+        distribuicao = session.query(Ocorrencia.id_tipo, count2).\
+            group_by(Ocorrencia.id_tipo).all()
+        distribuicao_tipo = []
+        for tipo, qt in distribuicao:
+            if tipo is not None:
+                if str(tipo).isdigit():
+                    tipo_oco = None
+
+                    if int(tipo) == 1:
+                        tipo_oco = 'assalto'
+                    elif int(tipo) == 2:
+                        tipo_oco = 'roubo'
+                    elif int(tipo) == 3:
+                        tipo_oco = 'sequestro'
+                    elif int(tipo) == 4:
+                        tipo_oco = 'arrombamento'
+                    elif int(tipo) == 5:
+                        tipo_oco = 'tiroteio'
+                    elif int(tipo) == 6:
+                        tipo_oco = 'homicidio'
+                    elif int(tipo) == 7:
+                        tipo_oco = 'trafico'
+                    elif int(tipo) == 8:
+                        tipo_oco = 'agressao'
+                    elif int(tipo) == 9:
+                        tipo_oco = 'estupro'
+                    elif int(tipo) == 10:
+                        tipo_oco = 'acidente'
+                    if tipo_oco is not None:
+                        distribuicao_tipo.append({'tipo': tipo_oco, 'quantidade': qt})
+
+        estatisticas['distribuicao_tipo'] = distribuicao_tipo
+
+        distribuicao = session.query(Ocorrencia.data).all()
+        distribuicao_ano = []
+        dict_distri_ano = {}
+        for ano in distribuicao:
+            ano = ano[0]
+            if ano is not None:
+                if ano.strip != '':
+                    valido = False
+                    if '-' in ano:
+                        ano = ano.split('-')[0]
+                        valido = True
+                    elif '/' in ano:
+                        ano = ano.split('/')[2]
+                        valido = True
+                    if valido:
+                        if ano not in dict_distri_ano:
+                            dict_distri_ano[ano] = 0
+                        else:
+                            dict_distri_ano[ano] += 1
+        for each in dict_distri_ano.keys():
+            distribuicao_ano.append({each: dict_distri_ano[each]})
+
+        estatisticas['distribuicao_ano'] = distribuicao_ano
+
+        return estatisticas
